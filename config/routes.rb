4a4194c2abf74255ac08c4832dc824a4a0cdea99
renysaldivar <renata.saldivar97@gmail.com>
2020-05-05 20:05:25 -0500

    TT-09_Renata agregar controladores de devise

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :professors
   resources :students
 
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
