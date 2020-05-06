0162f2969e29152e0768d32bc82c4aa129bdb28e
Renata <renata.saldivar97@gmail.com>
2020-05-05 20:14:20 -0500

    TT-09_Renata (#6)
    * TT-09_Renata agregar controladores de devise
    
    * TT-09_Renata agregar vistas del modelo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # Devise
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # Resources
+  resources :users
   resources :judge_evaluations
   resources :project_event_details
   resources :social_impacts
@@ -17,7 +24,4 @@ Rails.application.routes.draw do
   resources :judges
   resources :professors
   resources :students
-
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
