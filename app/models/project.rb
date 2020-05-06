e00e732829b8a97be6cb2aee5ea7cd4b44bf7073
Carlos <carlosfgarza@gmail.com>
2020-05-05 19:22:42 -0500

    TT-08_carlos added project relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
   belongs_to :professor
   belongs_to :institution
   belongs_to :edition
+  has_one :abstract
+  has_one :committee_evaluation
+  has_one :project_detail
 end
