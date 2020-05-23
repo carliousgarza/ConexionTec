b434056b9e51458bced223bcd8432d7e2d52b326
renysaldivar <renata.saldivar97@gmail.com>
2020-05-23 16:51:04 -0500

    HU-02_Renata agregar estilo a la vista de proyectos

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,67 @@
 module ProjectsHelper
+  def project_description(project)
+    project_detail = project.project_detail
+    project_detail.description
+  end
+
+  def project_name(project)
+    project_detail = project.project_detail
+    project_detail.name
+  end
+
+  def project_professor(project)
+    professor = project.professor
+    user = professor.user
+    user.full_name
+  end
+
+  def project_professor_email(project)
+    professor = project.professor
+    user = professor.user
+    user.email
+  end
+
+  def project_score(project)
+    committee_evaluation = project.committee_evaluation
+    committee_evaluation.score
+  end
+
+  def project_stand(project)
+    project_event_details = project.project_event_detail
+    project_event_details.stand
+  end
+
+  def project_student(project)
+    student = project.student
+    user = student.user
+    user.full_name
+  end
+
+  def project_student_email(project)
+    student = project.student
+    user = student.user
+    user.email
+  end
+
+  def display_project_professor_info(project)
+    project_professor = project_professor(project)
+    project_professor_email = project_professor_email(project)
+    return "#{project_professor} • #{project_professor_email}"
+  end
+
+  def display_project_score(project)
+    project_score = project_score(project)
+    project_score.present? ? project_score : "Pendiente"
+  end
+
+  def display_project_stand(project)
+    project_stand = project_stand(project)
+    project_stand.present? ? project_stand : "Pendiente"
+  end
+
+  def display_project_student_info(project)
+    project_student = project_student(project)
+    project_student_email = project_student_email(project)
+    return "#{project_student} • #{project_student_email}"
+  end
 end
