c863c11e278f30bb1cd1b5cbc2c28c2ee6e02257
renysaldivar <renata.saldivar97@gmail.com>
2020-05-24 17:30:32 -0500

    HU-03_Renata arreglar errores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_23_235458) do
 
   create_table "abstracts", force: :cascade do |t|
