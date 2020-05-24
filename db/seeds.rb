486f5c1890b3c5c892f1fcd13ded083dd040b88f
renysaldivar <renata.saldivar97@gmail.com>
2020-05-24 17:36:44 -0500

    HU-03_Renata agregar datos de profesor en el archivo de seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,18 @@ Edition.create(name: name, start_date: start_date, end_date: end_date)
 name = "Tecnológico de Monterrey Campus Monterrey"
 city = "Monterrey"
 Institution.create(name: name, city: city)
+
+# Professor
+department = "Ingeniería y Ciencias"
+Professor.create(department: department)
+
+# User
+email = "luis@tec.mx"
+password = "123123"
+first_name = "Luis"
+last_name = "Gonzalez"
+userable_id = 1
+userable_type = "Professor"
+edition_id = 1
+institution_id = 1
+User.create(email: email, password: password, first_name: first_name, last_name: last_name, userable_id: userable_id, userable_type: userable_type, edition_id: edition_id, institution_id: institution_id)
