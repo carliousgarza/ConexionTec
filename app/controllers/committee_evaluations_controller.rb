bb66c60da8266b5ba95ff2b41d33b80a1e18282a
renysaldivar <renata.saldivar97@gmail.com>
2020-06-05 16:29:37 -0500

    HU-04_Renata agregar forma para calificar un proyecto

--- app/controllers/committee_evaluations_controller.rb
+++ app/controllers/committee_evaluations_controller.rb
@@ -1,11 +1,13 @@
 class CommitteeEvaluationsController < ApplicationController
+  before_action :get_project
   before_action :set_committee_evaluation, only: [:show, :edit, :update, :destroy]
 
   # GET /committee_evaluations
   # GET /committee_evaluations.json
-  def index
-    @committee_evaluations = CommitteeEvaluation.all
-  end
+  # Resource (singular) in routing file does not include index
+  # def index
+  #   @committee_evaluations = CommitteeEvaluation.all
+  # end
 
   # GET /committee_evaluations/1
   # GET /committee_evaluations/1.json
@@ -14,7 +16,7 @@ class CommitteeEvaluationsController < ApplicationController
 
   # GET /committee_evaluations/new
   def new
-    @committee_evaluation = CommitteeEvaluation.new
+    @committee_evaluation = @project.build_committee_evaluation
   end
 
   # GET /committee_evaluations/1/edit
@@ -24,11 +26,11 @@ class CommitteeEvaluationsController < ApplicationController
   # POST /committee_evaluations
   # POST /committee_evaluations.json
   def create
-    @committee_evaluation = CommitteeEvaluation.new(committee_evaluation_params)
+    @committee_evaluation = @project.build_committee_evaluation(committee_evaluation_params)
 
     respond_to do |format|
       if @committee_evaluation.save
-        format.html { redirect_to @committee_evaluation, notice: 'Committee evaluation was successfully created.' }
+        format.html { redirect_to projects_path, notice: 'Committee evaluation was successfully created.' }
         format.json { render :show, status: :created, location: @committee_evaluation }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class CommitteeEvaluationsController < ApplicationController
   def update
     respond_to do |format|
       if @committee_evaluation.update(committee_evaluation_params)
-        format.html { redirect_to @committee_evaluation, notice: 'Committee evaluation was successfully updated.' }
+        format.html { redirect_to projects_path, notice: 'Committee evaluation was successfully updated.' }
         format.json { render :show, status: :ok, location: @committee_evaluation }
       else
         format.html { render :edit }
@@ -62,9 +64,13 @@ class CommitteeEvaluationsController < ApplicationController
   end
 
   private
+    def get_project
+      @project = Project.find(params[:project_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_committee_evaluation
-      @committee_evaluation = CommitteeEvaluation.find(params[:id])
+      @committee_evaluation = @project.committee_evaluation
     end
 
     # Only allow a list of trusted parameters through.
