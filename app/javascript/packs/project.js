9bb8efc06692aba02f4122e0242594e146a41945
renysaldivar <renata.saldivar97@gmail.com>
2020-06-05 18:27:36 -0500

    BUG_Renata agregar contador de palabras en el abstract

--- app/javascript/packs/project.js
+++ app/javascript/packs/project.js
@@ -8,3 +8,17 @@ window.manageSocialImpactForm = function() {
     socialImpactForm.style.display = "none";
   }
 }
+
+window.manageWordCount = function(textId, wordCountId) {
+  textValue = document.getElementById(textId).value;
+  wordCount = document.getElementById(wordCountId);
+  wordCount.value = countWords(textValue);
+}
+
+// https://www.tutorialspoint.com/how-to-count-a-number-of-words-in-given-string-in-javascript
+function countWords(str) {
+   str = str.replace(/(^\s*)|(\s*$)/gi,"");
+   str = str.replace(/[ ]{2,}/gi," ");
+   str = str.replace(/\n /,"\n");
+   return str.split(' ').length;
+ }
