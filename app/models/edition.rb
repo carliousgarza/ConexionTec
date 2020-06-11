9c6051902ecde1c19722fe88ab55859f159a7d22
renysaldivar <renata.saldivar97@gmail.com>
2020-06-10 20:07:17 -0500

    BUG_Renata_2 cambios en el modelo de ediciones

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -2,6 +2,6 @@ class Edition < ApplicationRecord
     has_many :users
     has_many :questions
     has_many :projects
-    has_many :time_limits
-    accepts_nested_attributes_for :time_limits
+    has_many :time_limits, dependent: :destroy
+    accepts_nested_attributes_for :time_limits, allow_destroy: true
 end
