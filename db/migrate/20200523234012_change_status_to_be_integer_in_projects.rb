3f75ae263e93bcc79d6f1df7639cf33fe96ca334
JimenaLomeli <jimena@lomeli.mx>
2020-06-10 22:17:21 -0500

    Deploy to heroku

--- db/migrate/20200523234012_change_status_to_be_integer_in_projects.rb
+++ db/migrate/20200523234012_change_status_to_be_integer_in_projects.rb
@@ -1,6 +1,6 @@
 class ChangeStatusToBeIntegerInProjects < ActiveRecord::Migration[6.0]
   def up
-    change_column :projects, :status, :integer
+    change_column :projects, :status, 'integer USING CAST(status AS integer)' 
   end
 
   def down
